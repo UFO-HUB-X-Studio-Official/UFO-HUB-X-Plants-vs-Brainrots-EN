4d60f5eaa4f21a6e7fdee84aa4f9baf1f07cffa6
UFO-HUB-X-Studio-Official <ufohubxstudio@gmail.com>
2025-09-29 12:56:18 +0700

    Update UFO-HUB-X-Plants-vs-Brainrots-EN.lua

--- UFO-HUB-X-Plants-vs-Brainrots-EN.lua
+++ UFO-HUB-X-Plants-vs-Brainrots-EN.lua
@@ -1,2 +1,2 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/UFO-HUB-X-Studio-Official/UFO-HUB-X-UI/refs/heads/main/UFO%20HUB%20X%20UI.lua"))()
-local Window = Library.CreateLib("UFO HUB X", "UFOHUBX")
+local Window = Library.CreateLib("TITLE", "DarkTheme")
